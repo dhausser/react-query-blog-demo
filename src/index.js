36623fcbdb3133500d05f02197e43849a9fe4878
Davy Hausser <davy.hausser@gmail.com>
2021-05-22 15:35:22 +0200

    Upgrade to react-query v3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 //
 
 import { Wrapper, Main } from './components/styled'
@@ -18,30 +20,35 @@ function SafeHydrate({ children }) {
   )
 }
 
+const queryClient = new QueryClient()
+
 export default function App() {
   return (
-    <SafeHydrate>
-      <BrowserRouter>
-        <Wrapper>
-          <Sidebar />
-          <Main>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <h1>Welcome!</h1>
-                  </>
-                }
-              />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/admin/:postId" element={<AdminPost />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:postId" element={<BlogPost />} />
-            </Routes>
-          </Main>
-        </Wrapper>
-      </BrowserRouter>
-    </SafeHydrate>
+    <QueryClientProvider client={queryClient}>
+      <SafeHydrate>
+        <BrowserRouter>
+          <Wrapper>
+            <Sidebar />
+            <Main>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <h1>Welcome!</h1>
+                    </>
+                  }
+                />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin/:postId" element={<AdminPost />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/blog/:postId" element={<BlogPost />} />
+              </Routes>
+            </Main>
+          </Wrapper>
+          <ReactQueryDevtools />
+        </BrowserRouter>
+      </SafeHydrate>
+    </QueryClientProvider>
   )
 }
