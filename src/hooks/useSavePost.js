8cb7511a90c61dafe2b03bca08c62509ef242b91
Davy Hausser <davy.hausser@gmail.com>
2021-05-23 08:56:05 +0200

    Save post using the fetch API

--- src/hooks/useSavePost.js
+++ src/hooks/useSavePost.js
@@ -1,49 +1,53 @@
-import axios from 'axios'
 import { useMutation, useQueryClient } from 'react-query'
+
+const savePost = (newPost) =>
+  fetch(`/api/posts/${newPost.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newPost),
+  })
 
 export default function useSavePost() {
   const queryClient = useQueryClient()
-  return useMutation(
-    (newPost) =>
-      axios.patch(`/api/posts/${newPost.id}`, newPost).then((res) => res.data),
-    {
-      // When mutate is called:
-      onMutate: async (newPost) => {
-        // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-        await queryClient.cancelQueries(['posts', newPost.id])
+  return useMutation(savePost, {
+    // When mutate is called:
+    onMutate: async (newPost) => {
+      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries(['posts', newPost.id])
 
-        // Snapshot the previous value
-        const previousPost = queryClient.getQueryData(['posts', newPost.id])
+      // Snapshot the previous value
+      const previousPost = queryClient.getQueryData(['posts', newPost.id])
 
-        // Optimistically update to the new value
-        queryClient.setQueryData(['posts', newPost.id], newPost)
-        if (queryClient.getQueryData('posts')) {
-          queryClient.setQueryData('posts', (old) => {
-            return old.map((d) => {
-              if (d.id == newPost.id) {
-                return newPost
-              }
-              return d
-            })
+      // Optimistically update to the new value
+      queryClient.setQueryData(['posts', newPost.id], newPost)
+      if (queryClient.getQueryData('posts')) {
+        queryClient.setQueryData('posts', (old) => {
+          return old.map((d) => {
+            if (d.id == newPost.id) {
+              return newPost
+            }
+            return d
           })
-        } else {
-          queryClient.setQueryData('posts', [newPost])
-        }
+        })
+      } else {
+        queryClient.setQueryData('posts', [newPost])
+      }
 
-        // Return a context object with the snapshotted value
-        return { previousPost, newPost }
-      },
-      // If the mutation fails, use the context returned from onMutate to roll back
-      onError: (err, newPost, context) => {
-        queryClient.setQueryData(
-          ['posts', context.newPost.id],
-          context.previousPost
-        )
-      },
-      // Always refetch after error or success:
-      onSettled: (newTodo) => {
-        queryClient.invalidateQueries(['posts', newTodo.id])
-      },
-    }
-  )
+      // Return a context object with the snapshotted value
+      return { previousPost, newPost }
+    },
+    // If the mutation fails, use the context returned from onMutate to roll back
+    onError: (err, newPost, context) => {
+      queryClient.setQueryData(
+        ['posts', context.newPost.id],
+        context.previousPost
+      )
+    },
+    // Always refetch after error or success:
+    onSettled: (newTodo) => {
+      queryClient.invalidateQueries(['posts', newTodo.id])
+    },
+  })
 }
