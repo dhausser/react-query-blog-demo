c466c648b34992f73f3cabacc34df342190d9650
Davy Hausser <davy.hausser@gmail.com>
2021-05-23 08:55:07 +0200

    Create post using the fetch API

--- src/hooks/useCreatePost.js
+++ src/hooks/useCreatePost.js
@@ -1,12 +1,23 @@
 import { useMutation, useQueryClient } from 'react-query'
-import axios from 'axios'
+
+const createPost = (newPost) =>
+  fetch('/api/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newPost),
+  })
 
 export default function useCreatePost() {
   const queryClient = useQueryClient()
-  return useMutation((newPost) => axios.post('/api/posts', newPost), {
+  return useMutation(createPost, {
     onMutate: async (newPost) => {
       // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
       await queryClient.cancelQueries('posts')
+
+      console.log(newPost)
+      console.log(JSON.stringify(newPost))
 
       // Snapshot the previous value
       const previousPosts = queryClient.getQueryData('posts')
