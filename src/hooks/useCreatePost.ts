fed607ee1a13ce5e71397f917d346aceeb08e861
Davy Hausser <davy.hausser@gmail.com>
2021-05-30 17:50:55 +0200

    Rename file to ts

--- src/hooks/useCreatePost.ts
+++ src/hooks/useCreatePost.ts
@@ -1,35 +1,43 @@
-import { useMutation, useQueryClient } from 'react-query'
+import {
+  useMutation,
+  MutationFunction,
+  UseMutationOptions,
+  useQueryClient,
+} from 'react-query'
+import { Post } from 'types'
 
-const createPost = (newPost) =>
+const createPost = (newPost): Promise<Post> =>
   fetch('/api/posts', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(newPost),
-  }).catch((error) => {
-    throw new Error(error)
   })
+    .then((res) => res.json())
+    .catch((error) => {
+      throw new Error(error)
+    })
 
 export default function useCreatePost() {
   const queryClient = useQueryClient()
   return useMutation(createPost, {
-    onMutate: async (newPost) => {
+    onMutate: async (newPost: Post) => {
       // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
       await queryClient.cancelQueries('posts')
 
       // Snapshot the previous value
-      const previousPosts = queryClient.getQueryData('posts')
+      const previousPosts = queryClient.getQueryData<Post[]>('posts')
 
       // Optimistically update to the new value
-      queryClient.setQueryData('posts', (old) => [...old, newPost])
+      queryClient.setQueryData<Post[]>('posts', (old) => [...old, newPost])
 
       // Return a context object with the snapshotted value
       return { previousPosts }
     },
     // If the mutation fails, use the context returned from onMutate to roll back
     onError: (err, newPost, context) => {
-      queryClient.setQueryData('posts', context.previousTodos)
+      queryClient.setQueryData('posts', context.previousPosts)
     },
     // Always refetch after error or success:
     onSettled: () => {
