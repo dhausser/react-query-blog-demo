752b626c9983cf13d11493b8297c6bd1114ced4f
Davy Hausser <davy.hausser@gmail.com>
2021-05-30 17:51:58 +0200

    Add renderHook to utils

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,9 +1,14 @@
 import { render as rtlRender } from '@testing-library/react'
+import { renderHook as rtlRenderHook } from '@testing-library/react-hooks'
 import { AppProviders } from 'context/app-providers'
 
 function render(ui, renderOptions = {}) {
   return rtlRender(ui, { wrapper: AppProviders, ...renderOptions })
 }
 
+function renderHook(customHook, renderOptions = {}) {
+  return rtlRenderHook(customHook, { wrapper: AppProviders, ...renderOptions })
+}
+
 export * from '@testing-library/react'
-export { render }
+export { render, renderHook }
