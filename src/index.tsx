5ad84aef21fc04a04687a9162fd501cf2a0a1b13
Davy Hausser <davy.hausser@gmail.com>
2021-05-30 15:28:00 +0200

    Move app providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { QueryClientProvider, QueryClient } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 //
 
+import { AppProviders } from 'context/app-providers'
 import { Wrapper, Main } from './components/styled'
 import GlobalLoader from './components/GlobalLoader'
 import Sidebar from './components/Sidebar'
@@ -13,50 +13,30 @@ import AdminPost from './screens/admin/Post'
 import Blog from './screens/blog'
 import BlogPost from './screens/blog/Post'
 
-function SafeHydrate({ children }) {
-  return (
-    <div suppressHydrationWarning>
-      {typeof document === 'undefined' ? null : children}
-    </div>
-  )
-}
-
-const queryClient = new QueryClient({
-  // defaultOptions: {
-  //   queries: {
-  //     staleTime: Infinity,
-  //   },
-  // },
-})
-
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeHydrate>
-        <BrowserRouter>
-          <Wrapper>
-            <GlobalLoader />
-            <Sidebar />
-            <Main>
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <>
-                      <h1>Welcome!</h1>
-                    </>
-                  }
-                />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/admin/:postId" element={<AdminPost />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/blog/:postId" element={<BlogPost />} />
-              </Routes>
-            </Main>
-          </Wrapper>
-          <ReactQueryDevtools />
-        </BrowserRouter>
-      </SafeHydrate>
-    </QueryClientProvider>
+    <AppProviders>
+      <Wrapper>
+        <GlobalLoader />
+        <Sidebar />
+        <Main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <h1>Welcome!</h1>
+                </>
+              }
+            />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin/:postId" element={<AdminPost />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:postId" element={<BlogPost />} />
+          </Routes>
+        </Main>
+      </Wrapper>
+      <ReactQueryDevtools />
+    </AppProviders>
   )
 }
